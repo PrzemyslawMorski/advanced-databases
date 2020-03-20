5ea28e24e73449f9715fc13a4a284a470e2a8f0a
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-03-20 13:44:10 +0100

    query - employees with sales higher than average

---
+++ load_tests/rental_set/queries/employees_with_sales_higher_than_average/query.sql
@@ -0,0 +1,30 @@
+with staff_sales_with_avg_sales as (
+    with avg_staff_sales as (
+	    with staff_sales as (
+    		select 
+	    		count(*) as num_sales
+		    from staff s
+		    left outer join rental r
+		    on s.staff_id = r.staff_id
+		    group by s.staff_id
+	    )
+	    select 
+    		avg(num_sales) as average_sales_per_staff
+    	from staff_sales
+    )
+    select 
+    	count(*) as num_sales,
+    	first_name,
+    	last_name,
+    	(select * from avg_staff_sales)
+    from staff s
+    left outer join rental r
+    on s.staff_id = r.staff_id
+    group by s.staff_id
+)
+select *
+from staff_sales_with_avg_sales
+where num_sales >= average_sales_per_staff
+
+
+
