763722ecdf76a4dbd725df199591151467561659
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-03-23 18:40:04 +0100

    reformatted query test scripts

--- query_runner/test_query_using_explain.sh
+++ query_runner/test_query_using_explain.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
 SUM_EXECUTION_TIME=$(awk "BEGIN {print 0.0; exit}")
 SUM_PLANNING_TIME=$(awk "BEGIN {print 0.0; exit}")
 
 counter=0
-while [ $counter -lt $NUM_TESTS ]
+while [ $counter -lt "$NUM_TESTS" ]
 do
     OUTPUT="$(psql -h dvd_rental_db -p 5432 -U postgres -d dvdrental -w -a -f /dvdrental/query.sql)"
     
-    EXECUTION_TIME="$(echo $OUTPUT | grep -o -e 'Execution Time: \S* ms' | grep -o -e '[0-9]*[.][0-9]*')"
-    PLANNING_TIME="$(echo $OUTPUT | grep -o -e 'Planning Time: \S* ms' | grep -o -e '[0-9]*[.][0-9]*')"
+    EXECUTION_TIME="$(echo "$OUTPUT" | grep -o -e 'Execution Time: \S* ms' | grep -o -e '[0-9]*[.][0-9]*')"
+    PLANNING_TIME="$(echo "$OUTPUT" | grep -o -e 'Planning Time: \S* ms' | grep -o -e '[0-9]*[.][0-9]*')"
     
     if [ -n "$EXECUTION_TIME" ]; then
         if [ -n "$EXECUTION_TIME" ]; then
             SUM_EXECUTION_TIME=$(awk "BEGIN {print $SUM_EXECUTION_TIME+$EXECUTION_TIME; exit}")
             SUM_PLANNING_TIME=$(awk "BEGIN {print $SUM_PLANNING_TIME+$PLANNING_TIME; exit}")
-            counter=$(( $counter + 1 ))
+            counter=$(( counter + 1 ))
         fi
     fi
 done
