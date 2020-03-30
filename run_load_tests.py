c7b0285222f7931e3100907a93bbbbe6b183e3a1
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-03-30 10:34:44 +0200

    run_load_tests.py runs all test queries

--- run_load_tests.py
+++ run_load_tests.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python
 
+from pathlib import Path
 import subprocess
 import os
+import glob
+import sys
+
+
+def restore_db():
+    print('Restoring database...')
+    output = subprocess.Popen(
+        ['docker-compose', 'run', 'migrator'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.Popen.wait(output)
 
 
 def run_test(query_path, num_tests):
+    print('Running: ' + query_path)
     os.system('run_query.py ' + query_path + ' ' + str(num_tests))
 
 
 # setup
-output = subprocess.Popen(['docker-compose', 'run', 'migrator'])
-subprocess.Popen.wait(output)
+# restore_db()
 
-# set - customer
-
-# queries
-
-# /dvdrental/load_test_queries/customer_set/queries/movies_rented_more_than_average/query.sql
-print('Running: load_test_queries/customer_set/queries/movies_rented_more_than_average/query.sql')
-
-run_test(os.path.join(
-    'load_test_queries', 'customer_set', 'queries', 'movies_rented_more_than_average', 'query.sql'), 10)
-
-# /dvdrental/load_test_queries/customer_set/queries/num_rentals_by_release_year/query.sql
-
-# /dvdrental/load_test_queries/customer_set/queries/rentals_from_a_city/query.sql
-
-# data manipulation
-
-# /dvdrental/load_test_queries/customer_set/data_manipulation/delete_movie/query.sql
-
-# /dvdrental/load_test_queries/customer_set/data_manipulation/delete_unused_categories/query.sql
-
-# set - rentals
-
-
-# set - store
+for path in glob.iglob('load_test_queries/**', recursive=True):
+    if (Path(path).name.endswith('.sql')):
+        run_test(path, 10)
