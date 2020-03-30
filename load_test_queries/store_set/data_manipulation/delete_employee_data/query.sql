58399c72ca40cfd6331a617c1861e2f3bec1eb6e
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-03-30 12:06:41 +0200

    delete employee data and delete store queries

--- load_test_queries/store_set/data_manipulation/delete_employee_data/query.sql
+++ load_test_queries/store_set/data_manipulation/delete_employee_data/query.sql
@@ -2,10 +2,43 @@ BEGIN;
 
 SAVEPOINT save;
 
+UPDATE
+    rental
+SET
+    staff_id = null
+FROM
+    rental r
+    left outer join staff c on r.staff_id = c.staff_id
+WHERE
+    r.rental_date < '2015-02-01 08:43:49'
+    and first_name like 'Geo%'
+    and last_name like 'Dut%'
+    and email like 'gdutson2br%'
+    and email like '%moonfruit%'
+    and email like '%.com';
+
+UPDATE
+    payment
+SET
+    staff_id = null
+FROM
+    payment p
+    left outer join staff c on p.staff_id = c.staff_id
+WHERE
+    first_name like 'Geo%'
+    and last_name like 'Dut%'
+    and email like 'gdutson2br%'
+    and email like '%moonfruit%'
+    and email like '%.com';
+
 delete from
     staff
 where
-    staff_id = 2001;
+    first_name like 'Geo%'
+    and last_name like 'Dut%'
+    and email like 'gdutson2br%'
+    and email like '%moonfruit%'
+    and email like '%.com';
 
 ROLLBACK TO SAVEPOINT save;
 
--- load_test_queries/store_set/data_manipulation/delete_store/query.sql
+++ load_test_queries/store_set/data_manipulation/delete_store/query.sql
@@ -2,11 +2,57 @@ BEGIN;
 
 SAVEPOINT save;
 
-delete from
-    store;
+with staff_to_delete as (
+    select
+        s.staff_id
+    from
+        staff s
+        join store st on st.store_id = s.store_id
+        join address ad on st.address_id = ad.address_id
+    where
+        district LIKE 'Cal'
+        and last_update < '2006-02-15 09:45:30'
+)
+update
+    payment
+set
+    staff_id = null
+where
+    staff_id in (
+        select
+            *
+        from
+            staff_to_delete
+    );
 
 delete from
-    staff;
+    staff
+where
+    staff_id in (
+        select
+            s.staff_id
+        from
+            staff s
+            join store st on st.store_id = s.store_id
+            join address ad on st.address_id = ad.address_id
+        where
+            district LIKE 'Cal'
+            and last_update < '2006-02-15 09:45:30'
+    );
+
+delete from
+    store
+where
+    store_id in (
+        select
+            store_id
+        from
+            store st
+            join address ad on st.address_id = ad.address_id
+        where
+            district LIKE 'Cal'
+            and last_update < '2006-02-15 09:45:30'
+    );
 
 ROLLBACK TO SAVEPOINT save;
 
