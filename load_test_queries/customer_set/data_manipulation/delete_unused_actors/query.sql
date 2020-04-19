8ca90c24a3b9f44d658b64b74684c442b02cc927
Tomasz Mosur <tomasz@mosur.net>
2020-04-19 15:43:26 +0200

    actor

---
+++ load_test_queries/customer_set/data_manipulation/delete_unused_actors/query.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+SAVEPOINT save;
+
+delete from
+    actors a
+where
+    a.actor_id not in (
+        select
+            actor_id
+        from
+            film_actor
+    );
+
+ROLLBACK TO SAVEPOINT save;
+
+COMMIT;
