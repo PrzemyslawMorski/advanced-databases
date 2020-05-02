cefd411e7d557ecda5835f1d3024ae922c4d5b4e
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-05-02 17:11:12 +0200

    op 4 tests

--- etap 3/testy/no_xml/op_4.sql
+++ etap 3/testy/no_xml/op_4.sql
@@ -3,11 +3,12 @@ BEGIN;
 SAVEPOINT save;
 
 DO $do$
-DECLARE 
-	old_title text := (select title from film where film_id = 1);
 BEGIN
-	UPDATE film SET title = 'New title' where film_id = 1;
-	insert into film_change_history (film_id, field_name, old_value, new_value) VALUES (1, 'title', old_title, 'New title');
+FOR i IN 1..1000 LOOP
+	UPDATE film SET title = 'New title' || i where film_id = 1;
+	insert into film_change_history (film_id, field_name, old_value, new_value) 
+	VALUES (1, 'title', (select title from film where film_id = 1), 'New title' || i);
+END LOOP;
 END $do$;
 
 ROLLBACK TO SAVEPOINT save;
--- etap 3/testy/xml/op_4.sql
+++ etap 3/testy/xml/op_4.sql
@@ -4,9 +4,11 @@ SAVEPOINT save;
 
 DO $do$
 BEGIN
+FOR i IN 1..1000 LOOP
+
 	UPDATE film 
 	SET 
-		title = 'New title3',
+		title = 'New title ' || i,
 		change_history_xml = REGEXP_REPLACE(
         	xmlroot(xmlElement(name changes,
                       xpath('/changes/change', change_history_xml)
@@ -14,11 +16,13 @@ BEGIN
 							   now() AS change_date,
                                'title' as field_name,
                                (select title from film where film_id = 1) as old_value,
-                               'New title3' as new_value))
+                               'New title ' || i as new_value))
                   ), version '1.0')::text,
         '<element>|</element>',
         '', 'g')::xml
 	WHERE film_id = 1;
+END LOOP;
+
 END $do$;
 
 ROLLBACK TO SAVEPOINT save;
