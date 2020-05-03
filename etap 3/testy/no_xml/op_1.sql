3e158c185c7edb6f270e7723c6c0c6856bb44cb5
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-05-03 09:54:54 +0200

    no xml op 1 uproszczone

--- etap 3/testy/no_xml/op_1.sql
+++ etap 3/testy/no_xml/op_1.sql
@@ -1,37 +1,14 @@
-with city_store_rental as (
-    select
-        c.city_id,
-        i.store_id,
-        r.rental_id
-    from
-        rental r
-        left outer join inventory i on i.inventory_id = r.inventory_id
-        left outer join film f on f.film_id = i.film_id
-        left outer join store s on s.store_id = i.store_id
-        left outer join address ad on s.address_id = ad.address_id
-        left outer join city c on c.city_id = ad.city_id
-    group by
-        c.city_id,
-        i.store_id,
-        r.rental_id
-)
 select
-    city,
-    address,
-    title,
-    first_name,
-    last_name,
-    email
+    c.city,
+    ad.address,
+    f.title,
+    cus.first_name || ' ' || cus.last_name as customer_name,
+    cus.email
 from
-    city_store_rental csr
-    left outer join city c on csr.city_id = c.city_id
-    left outer join store s on csr.store_id = s.store_id
+    rental r
+    left outer join inventory i on i.inventory_id = r.inventory_id
+    left outer join film f on f.film_id = i.film_id
+    left outer join store s on s.store_id = i.store_id
     left outer join address ad on s.address_id = ad.address_id
-    left outer join rental r on r.rental_id = csr.rental_id
-    left outer join inventory i on r.inventory_id = i.inventory_id
-    left outer join film f on i.film_id = f.film_id
-    left outer join customer cust on cust.customer_id = r.customer_id
-order by
-    city,
-    address,
-    title
+    left outer join city c on c.city_id = ad.city_id
+    left outer join customer cus on r.customer_id = cus.customer_id
