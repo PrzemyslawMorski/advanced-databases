b11509fe808f0b5ec2cd2f8f3ef7720bcc11f991
PrzemysławMorski <przemyslaw.morski@euvic.pl>
2020-05-24 09:18:39 +0200

    increase_area_of_influence_of_store

--- etap 4/queries/increase_area_of_influence_of_store.sql
+++ etap 4/queries/increase_area_of_influence_of_store.sql
@@ -1,21 +1,11 @@
-BEGIN;
-
-SAVEPOINT save;
-
-DO
-$do$
-BEGIN 
-
+with expand_by as (
+	with area_of_influence as (
+		select area_of_influence from store where store_id = 1
+	)
+	SELECT radius * 0.1 FROM ST_MinimumBoundingRadius((select area_of_influence::geometry from area_of_influence))
+)
 update store
-set area_of_influence = ST_Scale(area_of_influence::geometry, 0.91, 0.91)
+set area_of_influence = ST_MinimumBoundingCircle(ST_Expand(area_of_influence::geometry, (select * from expand_by)))
 where store_id = 1;
 
 select area_of_influence from store where store_id = 1;
-
-
-END
-$do$;
-
-ROLLBACK TO SAVEPOINT save;
-
-COMMIT;
