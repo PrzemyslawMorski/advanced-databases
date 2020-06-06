1863900a5a8bf87b2bab0f8f3173423fd2d247a7
Tomasz Mosur <tomasz@mosur.net>
2020-06-06 11:58:11 +0200

    add rental columnt relation to client

--- etap 5/seed/seed_rental_column_data.sql
+++ etap 5/seed/seed_rental_column_data.sql
@@ -1,4 +1,5 @@
-SELECT p.amount, p.payment_date, s.first_name, s.email, a.address, c.city, country.country, f.title, f.description, f.release_year 
+DROP TABLE IF EXISTS rental_column_storage;
+SELECT r.rental_id, r.customer_id, p.amount, p.payment_date, s.first_name, s.email, a.address, c.city, country.country, f.title, f.description, f.release_year 
 	INTO rental_column_storage
 	FROM public.rental r join public.inventory i on r.inventory_id=i.inventory_id
 	join public.film f on i.inventory_id=f.film_id
@@ -7,4 +8,8 @@ SELECT p.amount, p.payment_date, s.first_name, s.email, a.address, c.city, count
 	join public.store st on st.store_id=s.store_id
 	join public.address a on st.address_id=a.address_id
 	join public.city c on a.city_id=c.city_id
-	join public.country country on c.country_id=country.country_id
+	join public.country country on c.country_id=country.country_id;
+
+ALTER TABLE rental_column_storage 
+ADD CONSTRAINT customerid_fk FOREIGN KEY (customer_id) REFERENCES public.customer(customer_id);
+ALTER TABLE rental_column_storage ADD PRIMARY KEY (rental_id);
